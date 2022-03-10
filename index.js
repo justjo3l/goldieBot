a350b7079820713933a101b92cfbf3d585887778
justjo3l <comedyandcomputers@gmail.com>
2022-03-11 01:54:53 +1100

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 'use strict';
 
+const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
+const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
+
 // Imports dependencies and set up http server
 const
   express = require('express'),
