228f90a41344c8d924c0da6b5f41315f4ba515b1
justjo3l <comedyandcomputers@gmail.com>
2022-03-30 04:12:51 +1100

    Update web.js

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,13 +1,14 @@
 import express from "express";
-import homeController from "../controllers/homeController.js";
-import chatController from "../controllers/chatController.js";
+import handleWebHelloWorld from "../controllers/homeController.js";
+import postWebhook from "../controllers/chatController.js";
+import getWebhook from "../controllers/chatController.js";
 
 let router = express.Router();
 
 let initWebRoutes = (app) => {
-    router.get("/", homeController.handleWebHelloWorld);
-    router.post("/webhook", chatController.postWebhook);
-    router.get("/webhook", chatController.getWebhook);
+    router.get("/", handleWebHelloWorld);
+    router.post("/webhook", postWebhook);
+    router.get("/webhook", getWebhook);
     return app.use("/", router);
 };
 
