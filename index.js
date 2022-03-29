828c980f93646d9a8600cf0c8c671e79879709e9
justjo3l <comedyandcomputers@gmail.com>
2022-03-30 01:54:48 +1100

    Update index.js

--- index.js
+++ index.js
@@ -54,6 +54,7 @@ app.post('/webhook', (req, res) => {
         // Gets the message. entry.messaging is an array, but ß
         // will only ever contain one message, so we get index 0
         let webhook_event = entry.messaging[0];
+        console.log("Message Received");
         console.log(webhook_event);
       });
   
