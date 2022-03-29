d0b02f284f97a231c9d90b678a9d2d86152a9d7f
justjo3l <comedyandcomputers@gmail.com>
2022-03-30 04:36:29 +1100

    Update web.js

--- src/routes/web.js
+++ src/routes/web.js
@@ -2,8 +2,6 @@ const express = require("express");
 const homeController = require('../controllers/homeController.js');
 const chatController = require('../controllers/chatController.js');
 
-console.log(chatController);
-
 let router = express.Router();
 
 let initWebRoutes = (app) => {
