89b53cf6839fe7b529700e5bf3e167a71b8512bf
justjo3l <comedyandcomputers@gmail.com>
2022-03-30 04:34:36 +1100

    Update web.js

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,3 +1,4 @@
+const express = require("express");
 const homeController = require('../controllers/homeController.js');
 const chatController = require('../controllers/chatController.js');
 
@@ -6,7 +7,7 @@ console.log(chatController);
 let router = express.Router();
 
 let initWebRoutes = (app) => {
-    router.get("/", homeController.handleWebHelloWorld);
+    router.get("/", homeController.getHomepage);
     router.post("/webhook", chatController.postWebhook);
     router.get("/webhook", chatController.getWebhook);
     return app.use("/", router);
