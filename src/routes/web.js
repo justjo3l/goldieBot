385085a4c86b492cea90c206343990249e804927
justjo3l <comedyandcomputers@gmail.com>
2022-03-31 00:52:43 +1100

    Update web.js

--- src/routes/web.js
+++ src/routes/web.js
@@ -4,12 +4,38 @@ const chatController = require('../controllers/chatController.js');
 
 let router = express.Router();
 
+let postWebhook2 = (req, res) => {
+    let body = req.body;
+
+    console.log("PW runs");
+  
+    // Checks this is an event from a page subscription
+    if (body.object === 'page') {
+  
+      // Iterates over each entry - there may be multiple if batched
+      body.entry.forEach(function(entry) {
+  
+        // Gets the message. entry.messaging is an array, but ß
+        // will only ever contain one message, so we get index 0
+        let webhook_event = entry.messaging[0];
+        console.log("Message Received");
+        console.log(webhook_event);
+      });
+  
+      // Returns a '200 OK' response to all requests
+      res.status(200).send('EVENT_RECEIVED');
+    } else {
+      // Returns a '404 Not Found' if event is not from a page subscription
+      res.sendStatus(404);
+    }
+};
+
 let initWebRoutes = (app) => {
 
     console.log("IWR runs");
 
     router.get("/", homeController.getHomepage);
-    router.post("/webhook", chatController.postWebhook);
+    router.post("/webhook", postWebhook2);
     router.get("/webhook", chatController.getWebhook);
 
     console.log(router);
