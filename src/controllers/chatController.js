8840709d137dd96884623fb8feffe6401e9acfed
justjo3l <comedyandcomputers@gmail.com>
2022-04-01 22:34:54 +1100

    Update chatController.js

--- src/controllers/chatController.js
+++ src/controllers/chatController.js
@@ -94,8 +94,8 @@ function handleMessage(sender_psid, received_message) {
           "payload": {
             "template_type": "generic",
             "elements": [{
-              "title": shop_items[0][1],
-              "subtitle": shop_prices[0][1],
+              "title": "ok",//shop_items[0][1],
+              "subtitle": "ok",//shop_prices[0][1],
               "buttons": [
                 {
                   "type": "postback",
