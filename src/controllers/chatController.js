668350061949d6080e91c8a1a85f92c50853a25c
justjo3l <comedyandcomputers@gmail.com>
2022-04-09 19:58:13 +1000

    Update chatController.js

--- src/controllers/chatController.js
+++ src/controllers/chatController.js
@@ -189,7 +189,7 @@ function callSendAPI(sender_psid, response) {
     "method": "POST",
     "json": request_body
   }, (err, res, body) => {
-    if (!err) {
+    if (!err && sender_psid!=botPSID) {
       console.log('Message Sent!')
     } else {
       console.error("Unable to send message:" + err);
