e0b12d84d494e4ba134d6c45bb3b7b4cffa8f5f0
justjo3l <comedyandcomputers@gmail.com>
2022-04-09 19:48:50 +1000

    Update chatController.js

--- src/controllers/chatController.js
+++ src/controllers/chatController.js
@@ -30,22 +30,22 @@ let postWebhook = (req, res) => {
         // Gets the body of the webhook event
         let webhook_event = entry.messaging[0];
         if (entry.messaging[0].message === undefined) {
-          //console.log(webhook_event);
-          //console.log(webhook_event.message.text);
-        }
-
-
-        // Get the sender PSID
-        let sender_psid = webhook_event.sender.id;
-        console.log('Sender PSID: ' + sender_psid);
-
-        // Check if the event is a message or postback and
-        // pass the event to the appropriate handler function
-        if (webhook_event.message) {
-          handleMessage(sender_psid, webhook_event.message);        
-        } else if (webhook_event.postback) {
-          handlePostback(sender_psid, webhook_event.postback);
         } else {
+          console.log(webhook_event);
+          console.log(webhook_event.message.text);
+
+          // Get the sender PSID
+          let sender_psid = webhook_event.sender.id;
+          console.log('Sender PSID: ' + sender_psid);
+
+          // Check if the event is a message or postback and
+          // pass the event to the appropriate handler function
+          if (webhook_event.message) {
+            handleMessage(sender_psid, webhook_event.message);        
+          } else if (webhook_event.postback) {
+            handlePostback(sender_psid, webhook_event.postback);
+          } else {
+          }
         }
         
       });
