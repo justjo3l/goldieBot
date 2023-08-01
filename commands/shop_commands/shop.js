05ab03f2a8d5cd9768437ffc678752ce899d2644
justjo3l <comedyandcomputers@gmail.com>
2023-08-01 21:16:51 +1000

    Extend limit to 20

--- commands/shop_commands/shop.js
+++ commands/shop_commands/shop.js
@@ -11,10 +11,13 @@ export default function shop(senderID) {
         reply = "SHOP ITEMS:\n\n";
 
         items.forEach((item, index) => {
-            if (index < 10) {
+            if (index < 20) {
                 let itemName = item.item_data.name;
                 let itemPrice = String(item.item_data.variations[0].item_variation_data.price_money.amount);
                 itemPrice = itemPrice.slice(0, (itemPrice.length - 2)) + "." + itemPrice.slice((itemPrice.length - 2));
+                if (itemPrice[0] == ".") {
+                    itemPrice = "0" + itemPrice;
+                }
                 console.log(itemName);
                 console.log(itemPrice);
                 reply += itemName + " - $" + itemPrice + "\n";
