21f935424ed7004caa3bd4726ca92ba1ed9e7c81
justjo3l <comedyandcomputers@gmail.com>
2023-08-01 21:14:25 +1000

    Set shop to return first 10 products

--- commands/shop_commands/shop.js
+++ commands/shop_commands/shop.js
@@ -10,13 +10,15 @@ export default function shop(senderID) {
     getItems().then((items) => {
         reply = "SHOP ITEMS:\n\n";
 
-        items.forEach((item) => {
-            let itemName = item.item_data.name;
-            let itemPrice = String(item.item_data.variations[0].item_variation_data.price_money.amount);
-            itemPrice = itemPrice.slice(0, 1) + "." + itemPrice.slice(1);
-            console.log(itemName);
-            console.log(itemPrice);
-            reply += itemName + " - $" + itemPrice + "\n";
+        items.forEach((item, index) => {
+            if (index < 10) {
+                let itemName = item.item_data.name;
+                let itemPrice = String(item.item_data.variations[0].item_variation_data.price_money.amount);
+                itemPrice = itemPrice.slice(0, (itemPrice.length - 2)) + "." + itemPrice.slice((itemPrice.length - 2));
+                console.log(itemName);
+                console.log(itemPrice);
+                reply += itemName + " - $" + itemPrice + "\n";
+            }
         });
     
         replySender(reply, senderID);
