0837a4477abe0e13739e3b414533b782f7b4a18e
justjo3l <comedyandcomputers@gmail.com>
2023-10-19 19:08:19 +0700

    Reverted axios changes in postback.js

--- processes/postback.js
+++ processes/postback.js
@@ -1,7 +1,7 @@
 // FILE TO HANDLE POSTBACK PROCESSING
 // NOT PROPERLY IMPLEMENTED YET
 
-import axios from 'axios';
+import request from 'request';
 import sendMessage from '../templates/sendMessage.js';
 
 /**
@@ -12,10 +12,11 @@ export default function processPostback(event) {
   const senderID = event.sender.id;
   const payload = event.postback.payload;
   if (payload === 'WELCOME') {
-    axios.get({url: 'https://graph.facebook.com/v3.3/' + senderID,
-      params: {access_token: process.env.PAGE_ACCESS_TOKEN,
+    request({url: 'https://graph.facebook.com/v3.3/' + senderID,
+      qs: {access_token: process.env.PAGE_ACCESS_TOKEN,
         fields: 'first_name',
       },
+      method: 'GET',
     }, function(error, response, body) {
       let greeting = '';
       if (error) {
