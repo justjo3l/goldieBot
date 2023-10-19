9b08ec75b1d48b41678ada7370aa7a1a11f86329
justjo3l <comedyandcomputers@gmail.com>
2023-10-19 19:36:12 +0700

    Fix unknown.js

--- commands/unknown_commands/unknown.js
+++ commands/unknown_commands/unknown.js
@@ -13,7 +13,7 @@ export default function unknown(command, senderID) {
   let reply = '';
 
   // Sending a GET request to get user's first name
-  axios.get('https://graph.facebook.com/v3.3/' + senderID, {
+  axios.get('https://graph.facebook.com/v3.3/' + senderID, {}, {
     params: {
       access_token: process.env.PAGE_ACCESS_TOKEN,
       fields: 'first_name',
